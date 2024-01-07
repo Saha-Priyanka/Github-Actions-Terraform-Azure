50205a35b4eb50166b1566001a42e84c182935b3
PRIYANKA SAHA <114468969+Saha-Priyanka@users.noreply.github.com>
2024-01-07 22:11:41 +0530

    Update main.tf

--- terraform/Infrastructure_Creation/main.tf
+++ terraform/Infrastructure_Creation/main.tf
@@ -15,7 +15,7 @@ terraform {
     backend "azurerm" {
         resource_group_name = "GitHub_RG"
         storage_account_name = "githubstorageacc"
-        container_name = "tfstate"
+        container_name = "tfstate1"
         key = "terraform.tfstate"
         arm_subscription_id = "793729a4-a745-4e4f-8b8d-88bda288a86a"
         arm_tenant_id = "1e9cc706-c3fd-4b8c-9dbd-a073e7384b56"
